9836ed762bc57d174b72ec40dfe66f8cbb12bcb0
Di Ye <32302461+diye2@users.noreply.github.com>
2020-06-18 13:00:47 -0400

    Add files via upload

--- Network Diagram & Timeline.R
+++ Network Diagram & Timeline.R
@@ -1,14 +1,13 @@
-# 2020-02-21
+# 2020-06-18
 
 # load packages
 library(igraph)
-library(statnet)
-library(ndtv) 
 library(ggplot2)
 library(tidyverse)
 
-nodes = read.csv("cleaned data/nodes-2020-02-21.csv", header = TRUE, as.is=TRUE)
-edges = read.csv("cleaned data/edges-2020-02-21.csv", header = TRUE, as.is=TRUE)
+# read data
+nodes = read.csv("cleaned data/nodes.csv", header = TRUE, as.is=TRUE)
+edges = read.csv("cleaned data/edges.csv", header = TRUE, as.is=TRUE)
 names(nodes)[1] = "ID"
 m=as.matrix(edges)
 
@@ -37,12 +36,17 @@ dynamicedges = edges %>% filter(from %in% dynamicnodes$ID) %>% filter(to %in% dy
 # output = dynamicnodes[unreasonable.idx,]
 # write.csv(output, "unreasonable year.csv", row.names = FALSE)
 
-################
-# full network #
-################
+###########################
+# FULL NETWORK (FIGURE 4) #
+###########################
 net <- graph_from_data_frame(dynamicedges, dynamicnodes, directed = T)
+# color
 V(net)$color = ifelse((nchar(as.character(V(net)$name)) <= 4), 
-                      ifelse(V(net)$name == "A000", "black", "dimgrey"), "lightgrey")
+                      ifelse(V(net)$name == "A000", "black", "blue"), "red")
+# shape
+V(net)$shape = ifelse((nchar(as.character(V(net)$name)) <= 4),
+                      ifelse(V(net)$name == "A000", 'sphere', 'square'), 'circle')
+# size
 V(net)$size = ifelse((nchar(as.character(V(net)$name)) <= 4), 
                      ifelse(V(net)$name == "A000", 10, 3), 1.5)
 
@@ -56,7 +60,7 @@ plot(net, layout = layout.fruchterman.reingold, rescale = TRUE,
 
 
 ########################
-# dynamic full network #
+# DYNAMIC FULL NETWORK #
 ########################
 ## dynamic network diagram (both, second-generation articles added each year)
 
@@ -76,8 +80,12 @@ dynamic = function(y) {
   n = dynamicnodes[which(dynamicnodes$Year == y | (nchar(as.character(dynamicnodes$ID)) <= 4) & dynamicnodes$Year <= y),]
   e = edges[((edges$from %in% n$ID) & (edges$to %in% n$ID)),]
   net = graph_from_data_frame(e, n, directed = T)
+  # color
   V(net)$color = ifelse((nchar(as.character(V(net)$name)) <= 4), 
-                        ifelse(V(net)$name == "A000", "black", "lightblue"), "mediumpurple")
+                        ifelse(V(net)$name == "A000", "black", "blue"), "red")
+  # shape
+  V(net)$shape = ifelse((nchar(as.character(V(net)$name)) <= 4),
+                        ifelse(V(net)$name == "A000", 'sphere', 'square'), 'circle')
   num_first_generation = sum(nchar(as.character(V(net)$name)) <= 4) - 1
   num_second_generation = sum(nchar(as.character(V(net)$name)) > 4)
   V(net)$size = ifelse((nchar(as.character(V(net)$name)) <= 4), 
@@ -116,9 +124,14 @@ specific = c("F000", "F002", "F003", "F004", "F008", "F009", "F013", "F014", "F0
               "F123", "F124", "F125", "F130", "F131", "F138", "F140", "F202", "F204", "F205", 
               "F206", "F207", "F208", "F209", "F302", "F303", "F400", "F403", "F405", "F406", "A000")
 
-############################
-# specific dynamic network #
-############################
+#2010-2019 specific citations
+post_specific = c("F009", "F015", "F026", "F030", "F045", "F047", "F057", "F059", "F063",
+                  "F064", "F075", "F078", "F083", "F094", "F102", "F106", "F118", "F119",
+                  "F120", "F122", "F123", "F124", "F125", "F130", "F140", "F204", "F205", 
+                  "F207", "F208", "F209", "F303", "F400", "F403", "F405", "F406", "A000")
+#######################################
+# SPECIFIC DYNAMIC NETWORK (FIGURE 5) #
+#######################################
 ## dynamic network diagram (both, second-generation articles added each year)
 
 dynamic_specific = function(y, dynamicnodes) {
@@ -137,13 +150,18 @@ dynamic_specific = function(y, dynamicnodes) {
   n = dynamicnodes[which(dynamicnodes$Year == y | (nchar(as.character(dynamicnodes$ID)) <= 4) & dynamicnodes$Year <= y),]
   e = edges[((edges$from %in% n$ID) & (edges$to %in% n$ID)),]
   net = graph_from_data_frame(e, n, directed = T)
-  V(net)$color = ifelse((nchar(as.character(V(net)$name)) <= 4),
-                        ifelse(V(net)$name == "A000", "black", "lightblue"), "mediumpurple")
+  # color
+  V(net)$color = ifelse((nchar(as.character(V(net)$name)) <= 4), 
+                        ifelse(V(net)$name == "A000", "black", "blue"), "red")
+  # shape
+  V(net)$shape = ifelse((nchar(as.character(V(net)$name)) <= 4),
+                        ifelse(V(net)$name == "A000", 'sphere', 'square'), 'circle')
+  
   num_first_generation = sum(as.character(V(net)$name) %in% specific) - 1
   other_first_generation = sum((!(as.character(V(net)$name) %in% specific) & nchar(V(net)$name) == 4))
   num_second_generation = sum(nchar(as.character(V(net)$name)) > 4)
-  V(net)$size = ifelse((nchar(as.character(V(net)$name)) <= 4), 
-                       ifelse(V(net)$name == "A000", 20, 6), 3)
+  V(net)$size = ifelse((nchar(as.character(V(net)$name)) <= 4),
+                       ifelse(V(net)$name == "A000", 20, 7), 5)
   set.seed(30)
   p = plot(net, layout = layout.fruchterman.reingold, rescale = TRUE,
            edge.arrow.size = 0.1,
@@ -159,9 +177,6 @@ dynamic_specific = function(y, dynamicnodes) {
   return(p)
 }
 
-# snodes = dynamicnodes[((dynamicnodes$ID %in% specific) | substr(as.character(dynamicnodes$ID), 1, 4) %in% specific),]
-# sedges = edges[((edges$from %in% snodes$ID) | (edges$to %in% snodes$ID)),]
-
 sedges = edges[((edges$to %in% specific[1:length(post_specific)-1]) | (edges$to %in% specific & edges$from == 'A000')),]
 snodes = dynamicnodes[(dynamicnodes$ID %in% sedges$from | dynamicnodes$ID %in% sedges$to | dynamicnodes$ID %in% specific),]
 
@@ -174,9 +189,10 @@ for(i in 2009:2019) {
   dynamic_specific(i, snodes)
 }
 
-#############################
-# specific whole sg network #
-#############################
+
+########################################
+# SPECIFIC WHOLE SG NETWORK (FIGURE 8) #
+########################################
 # 2019
 dynamic_specific = function(y, dynamicnodes, seed_number) {
   freq_table = dynamicnodes %>%
@@ -194,8 +210,12 @@ dynamic_specific = function(y, dynamicnodes, seed_number) {
   n = dynamicnodes[which(dynamicnodes$Year <= y),]
   e = edges[((edges$from %in% n$ID) & (edges$to %in% n$ID)),]
   net = graph_from_data_frame(e, n, directed = T)
-  V(net)$color = ifelse((nchar(as.character(V(net)$name)) <= 4),
-                        ifelse(V(net)$name == "A000", "black", "lightblue"), "mediumpurple")
+  # color
+  V(net)$color = ifelse((nchar(as.character(V(net)$name)) <= 4), 
+                        ifelse(V(net)$name == "A000", "black", "blue"), "red")
+  # shape
+  V(net)$shape = ifelse((nchar(as.character(V(net)$name)) <= 4),
+                        ifelse(V(net)$name == "A000", 'sphere', 'square'), 'circle')
   num_first_generation = sum(as.character(V(net)$name) %in% specific) - 1
   other_first_generation = sum((!(as.character(V(net)$name) %in% specific) & nchar(V(net)$name) == 4))
   num_second_generation = sum(nchar(as.character(V(net)$name)) > 4)
@@ -228,16 +248,9 @@ dynamic_specific(2019, snodes, 30)
 
 
 
-#2010-2019 specific citations
-post_specific = c("F009", "F015", "F026", "F030", "F045", "F047", "F057", "F059", "F063",
-                  "F064", "F075", "F078", "F083", "F094", "F102", "F106", "F118", "F119",
-                  "F120", "F122", "F123", "F124", "F125", "F130", "F140", "F204", "F205", 
-                  "F207", "F208", "F209", "F303", "F400", "F403", "F405", "F406", "A000")
-
-
-####################################
-# post retraction specific network #
-####################################
+###############################################
+# POST RETRACTION SPECIFIC NETWORK (FIGURE 9) #
+###############################################
 dynamic_post_specific = function(y, dynamicnodes, seed_number) {
   freq_table = dynamicnodes %>%
     filter(Year <= y) %>%
@@ -254,8 +267,12 @@ dynamic_post_specific = function(y, dynamicnodes, seed_number) {
   n = dynamicnodes[which(dynamicnodes$Year <= y),]
   e = edges[((edges$from %in% n$ID) & (edges$to %in% n$ID)),]
   net = graph_from_data_frame(e, n, directed = T)
-  V(net)$color = ifelse((nchar(as.character(V(net)$name)) <= 4),
-                        ifelse(V(net)$name == "A000", "black", "lightblue"), "mediumpurple")
+  # color
+  V(net)$color = ifelse((nchar(as.character(V(net)$name)) <= 4), 
+                        ifelse(V(net)$name == "A000", "black", "blue"), "red")
+  # shape
+  V(net)$shape = ifelse((nchar(as.character(V(net)$name)) <= 4),
+                        ifelse(V(net)$name == "A000", 'sphere', 'square'), 'circle')
   num_first_generation = sum(as.character(V(net)$name) %in% post_specific) - 1
   other_first_generation = sum((!(as.character(V(net)$name) %in% post_specific) & nchar(V(net)$name) == 4))
   num_second_generation = sum(nchar(as.character(V(net)$name)) > 4)
@@ -276,12 +293,16 @@ dynamic_post_specific = function(y, dynamicnodes, seed_number) {
   return(p)
 }
 
-# of specific first-generation articles: XX
-# other first-generation articles (citing): XX
-# of second-generation articles: XX
 
 sedges = edges[((edges$to %in% post_specific[1:length(post_specific)-1]) | (edges$to %in% post_specific & edges$from == 'A000')),]
 snodes = dynamicnodes[(dynamicnodes$ID %in% sedges$from | dynamicnodes$ID %in% sedges$to | dynamicnodes$ID %in% post_specific),]
+par(mfrow = c(1, 1))
+dynamic_post_specific(2019, snodes, 30)
+
+
+
+
+
 
 # correct check
 sedges$check = ifelse(as.character(sedges$to) == substr(as.character(sedges$from), 1, 4), 1, 0)
@@ -296,100 +317,10 @@ length(unique(sedges$from))
 # total sg citations
 length(sedges$from[nchar(sedges$from) != 4])
 
-par(mfrow = c(1, 1))
-dynamic_post_specific(2019, snodes, 30)
-
-
 
 #######################
-# Jodi's presentation #
+# TIMELINE (FIGURE 2) #
 #######################
-# The first-generation network by itself, year by year (or if that’s hard - just 2008 and 2019)
-dynamic = function(y) {
-  freq_table = dynamicnodes %>% 
-    filter(Year <= y) %>% 
-    group_by(group = substr(ID, 1, 4)) %>% 
-    summarise(Freq = n()) 
-  if (y >=2007) {
-    labels = freq_table %>% 
-      filter(Freq > 1 | group == "A000")
-  } else {
-    labels = freq_table %>% 
-      filter(Freq >= 1 | group == "A000")
-  }
-  
-  n = dynamicnodes[which((nchar(as.character(dynamicnodes$ID)) <= 4) & dynamicnodes$Year <= y),]
-  e = edges[((edges$from %in% n$ID) & (edges$to %in% n$ID)),]
-  net = graph_from_data_frame(e, n, directed = T)
-  V(net)$color = ifelse((nchar(as.character(V(net)$name)) <= 4), 
-                        ifelse(V(net)$name == "A000", "black", "lightblue"), "mediumpurple")
-  num_first_generation = sum(nchar(as.character(V(net)$name)) <= 4) - 1
-  V(net)$size = ifelse((nchar(as.character(V(net)$name)) <= 4), 
-                       ifelse(V(net)$name == "A000", 20, 6), 3)
-  set.seed(30)
-  p = plot(net, layout = layout.fruchterman.reingold, rescale = TRUE,
-           edge.arrow.size = 0.1, 
-           vertex.label.dist = 3,
-           vertex.label = NA,
-           main = as.character(y),
-           sub = paste0("Matsuyama Paper (black)\n", "# of first-generation articles (blue): ", 
-                        num_first_generation))
-  return(p)
-}
-par(mfrow = c(3, 3))
-for(i in 2006:2008) {
-  dynamic(i)
-}
-par(mfrow = c(3, 3))
-for(i in 2009:2019) {
-  dynamic(i)
-}
-
-# The WHOLE second-generation network, year by year (or if that’s hard - just 2008 and 2019)
-dynamic = function(y) {
-  freq_table = dynamicnodes %>% 
-    filter(Year <= y) %>% 
-    group_by(group = substr(ID, 1, 4)) %>% 
-    summarise(Freq = n()) 
-  if (y >=2007) {
-    labels = freq_table %>% 
-      filter(Freq > 1 | group == "A000")
-  } else {
-    labels = freq_table %>% 
-      filter(Freq >= 1 | group == "A000")
-  }
-  
-  n = dynamicnodes[which(dynamicnodes$Year <= y),]
-  e = edges[((edges$from %in% n$ID) & (edges$to %in% n$ID)),]
-  net = graph_from_data_frame(e, n, directed = T)
-  V(net)$color = ifelse((nchar(as.character(V(net)$name)) <= 4), 
-                        ifelse(V(net)$name == "A000", "black", "lightblue"), "mediumpurple")
-  num_first_generation = sum(nchar(as.character(V(net)$name)) <= 4) - 1
-  num_second_generation = sum(nchar(as.character(V(net)$name)) > 4)
-  V(net)$size = ifelse((nchar(as.character(V(net)$name)) <= 4), 
-                       ifelse(V(net)$name == "A000", 20, 6), 3)
-  set.seed(30)
-  p = plot(net, layout = layout.fruchterman.reingold, rescale = TRUE,
-           edge.arrow.size = 0.1, 
-           vertex.label.dist = 3,
-           vertex.label = NA,
-           main = as.character(y),
-           sub = paste0("Matsuyama Paper (black)\n", "# of first-generation articles (blue): ", 
-                        num_first_generation, 
-                        "\n# of second-generation articles (purple): ", 
-                        num_second_generation))
-  return(p)
-}
-par(mfrow = c(3, 3))
-for(i in 2006:2008) {
-  dynamic(i)
-}
-par(mfrow = c(3, 3))
-for(i in 2009:2019) {
-  dynamic(i)
-}
-
-# timeline up to 2019
 library(ggplot2)
 library(tidyverse)
 time_nodes = nodes %>% filter(nchar(ID) == 4) %>% filter(ID != "A000") %>% group_by(Year) %>% summarise(Freq = n())
